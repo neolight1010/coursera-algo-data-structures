7fe3881aef41c4dba6fea1b145f5df7fb27777c5
Anthony Suárez <neolight1010@gmail.com>
2021-04-03 23:05:32 -0500

    Implement get_fibonacci_last_digit_fast() and test()

--- algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
@@ -16,9 +17,37 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+int get_fibonacci_last_digit_fast(int n) {
+    if (n <= 1) return n;
+
+    int previous = 0;
+    int current = 1;
+
+    for (int i=0; i < n-1; i++) {
+        int temp_current = current;
+        current = (previous + current) % 10;
+        previous = temp_current;
+    }
+
+    return current;
+}
+
+void test() {
+    assert(get_fibonacci_last_digit_fast(3) == 2);
+    assert(get_fibonacci_last_digit_fast(331) == 9);
+    assert(get_fibonacci_last_digit_fast(327305) == 5);
+
+    std::cout << "Test finished successfully";
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+
+    // int c = get_fibonacci_last_digit_naive(n);
+    // test();
+    int c = get_fibonacci_last_digit_fast(n);
+
     std::cout << c << '\n';
-    }
+    return 0;
+}
