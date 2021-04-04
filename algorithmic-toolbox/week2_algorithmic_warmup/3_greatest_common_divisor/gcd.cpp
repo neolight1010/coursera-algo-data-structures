d8e0d661479c598d453d88bbb591e43624a0f2c9
Anthony Suárez <neolight1010@gmail.com>
2021-04-03 23:31:53 -0500

    Implement gcd tests

--- algorithmic-toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ algorithmic-toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 int gcd_naive(int a, int b) {
   int current_gcd = 1;
@@ -12,9 +13,33 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int gcd_fast(int a, int b) {
+
+}
+
+int test_solution(int n_tests = 50, int max=200) {
+  assert(gcd_fast(18, 35) == 1);
+  assert(gcd_fast(28851538, 1183019) == 17657);
+
+  // Stress test
+  for (int i=0; i < n_tests; i++) {
+    int rand_a = rand() % max;
+    int rand_b = rand() % max;
+
+    std::cout << rand_a << " " << rand_b;
+
+    assert(gcd_naive(rand_a, rand_b) == gcd_fast(rand_a, rand_b));
+  }
+
+  std::cout << "Test ended successfully";
+}
+
 int main() {
   int a, b;
-  std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  // std::cin >> a >> b;
+
+  // std::cout << gcd_naive(a, b) << std::endl;
+  test_solution();
+
   return 0;
 }
