d669fd825eccd2b0b2a5b5f2a8743471364c7b63
Anthony Suárez <neolight1010@gmail.com>
2021-04-07 12:47:58 -0500

    Write fibonacci_sum_last_digit tests.

--- algorithmic-toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ algorithmic-toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 int fibonacci_sum_naive(long long n) {
     if (n <= 1)
@@ -18,8 +19,37 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+int fibonacci_sum_fast(long long n) {
+    return 0;
+}
+
+void test_solution(int n_tests = 40, int max_n = 50) {
+    std::cout << "- " << 3 << ": " << fibonacci_sum_fast(3) << std::endl;
+    assert(fibonacci_sum_fast(3) == 4);
+    
+    std::cout << "- " << 100 << ": " << fibonacci_sum_fast(100) << std::endl;
+    assert(fibonacci_sum_fast(100) == 5);
+
+    // Stress test
+    for (int i=0; i < n_tests; i++) {
+        int rand_n = rand() % max_n + 1;
+
+        int naive_sol = fibonacci_sum_naive(rand_n);
+        int fast_sol = fibonacci_sum_fast(rand_n);
+
+        std::cout << "- " << rand_n << ": " << naive_sol << " " << fast_sol;
+    }
+
+    std::cout << "Test ended successfully";
+}
+
 int main() {
-    long long n = 0;
-    std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    // long long n = 0;
+    // std::cin >> n;
+    
+    // std::cout << fibonacci_sum_naive(n);
+    test_solution();
+    // std::cout << fibonacci_sum_fast(n);
+
+    return 0;
 }
