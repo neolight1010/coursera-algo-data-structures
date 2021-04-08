d8aebc7b78b8133a0138c6ca18945e69bcd1e95a
Anthony Suárez <neolight1010@gmail.com>
2021-04-07 21:25:51 -0500

    Implement fibonacci_partial_sum fast solution

--- algorithmic-toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ algorithmic-toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -21,30 +21,47 @@ int get_fibonacci_partial_sum_naive(long long from, long long to) {
 }
 
 int get_fibonacci_partial_sum_fast(long long from, long long to) {
-    return 0;
+    from = from % 60;
+    to = from + (to - from) % 60;
+    int sum = 0;
+
+    int previous = 0;
+    int current = 1;
+
+    for (int i=1; i < to + 1; i++) {
+        if (i >= from)
+            sum = (sum + current) % 10;
+
+        int temp_current = current;
+        current = (previous + current) % 10;
+        previous = temp_current;
+    }
+
+    return sum;
 }
 
 void test_solution(int n_tests = 40, int max = 60) {
-    std::cout << "- " << 3 << ", " << 7 << ": " << get_fibonacci_partial_sum_fast(3, 7);
+    std::cout << "- " << 3 << ", " << 7 << ": " << get_fibonacci_partial_sum_fast(3, 7) << std::endl;
     assert(get_fibonacci_partial_sum_fast(3, 7) == 1);
     
-    std::cout << "- " << 10 << ", " << 10 << ": " << get_fibonacci_partial_sum_fast(10, 10);
+    std::cout << "- " << 10 << ", " << 10 << ": " << get_fibonacci_partial_sum_fast(10, 10) << std::endl;
     assert(get_fibonacci_partial_sum_fast(10, 10) == 5);
 
 
-    std::cout << "- " << 10 << ", " << 200 << ": " << get_fibonacci_partial_sum_fast(10, 200);
+    std::cout << "- " << 10 << ", " << 200 << ": " << get_fibonacci_partial_sum_fast(10, 200) << std::endl;
     assert(get_fibonacci_partial_sum_fast(10, 200) == 2);
 
     // Stress test
     for (int i=0; i < n_tests; i++) {
-        long long rand_to = rand() % max;
-        long long rand_from = rand() % rand_to;
+        long long rand_to = rand() % (max - 1) + 1;
+        long long rand_from = rand() % (rand_to - 1) + 1;
+
+        std::cout << "- " << rand_from << ", " << rand_to << std::endl;
 
         int naive_sol = get_fibonacci_partial_sum_naive(rand_to, rand_from);
         int fast_sol = get_fibonacci_partial_sum_fast(rand_to, rand_from);
 
-        std::cout << "- " << rand_from << ", " << rand_to << ": " <<
-            naive_sol << " " << fast_sol << std::endl;
+        std::cout << "   " << naive_sol << " " << fast_sol << std::endl;
 
         assert(naive_sol == fast_sol);
     }
@@ -52,12 +69,12 @@ void test_solution(int n_tests = 40, int max = 60) {
 }
 
 int main() {
-    // long long from, to;
-    // std::cin >> from >> to;
+    long long from, to;
+    std::cin >> from >> to;
 
     // std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
-    test_solution();
-    // std::cout << get_fibonacci_partial_sum_fast(from, to) << std::endl;
+    // test_solution();
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << std::endl;
 
     return 0;
 }
