d95b04789a5f79548566ce5f5b286c0383ae40bd
Anthony Suárez <neolight1010@gmail.com>
2021-04-07 21:53:54 -0500

    Write fibonacci_sum_squares tests

--- algorithmic-toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ algorithmic-toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 int fibonacci_sum_squares_naive(long long n) {
     if (n <= 1)
@@ -18,8 +19,39 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+int fibonacci_sum_squares_fast(long long n) {
+    return 0;
+}
+
+void test_solution(int n_tests = 90, int max_n = 100) {
+    std::cout << "- 7: " << fibonacci_sum_squares_fast(7) << std::endl;
+    assert(fibonacci_sum_squares_fast(7) == 3);
+
+    std::cout << "- 73: " << fibonacci_sum_squares_fast(73) << std::endl;
+    assert(fibonacci_sum_squares_fast(73) == 1);
+
+    std::cout << "- 1234567890: " << fibonacci_sum_squares_fast(1234567890) << std::endl;
+    assert(fibonacci_sum_squares_fast(1234567890) == 0);
+
+    // Stress test
+    for (int i=0; i < n_tests; i++) {
+        long long rand_n = rand() % max_n;
+
+        int naive_sol = fibonacci_sum_squares_naive(rand_n);
+        int fast_sol = fibonacci_sum_squares_fast(rand_n);
+
+        std::cout << "- " << rand_n << ": " << naive_sol << " " << fast_sol << std::endl;
+        assert(naive_sol == fast_sol);
+    }
+
+    std::cout << "Test ended successfully!" << std::endl;
+}
+
 int main() {
-    long long n = 0;
-    std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    // long long n = 0;
+    // std::cin >> n;
+
+    // std::cout << fibonacci_sum_squares_naive(n);
+    test_solution();
+    // std::cout << fibonacci_sum_squares_fast(n);
 }
