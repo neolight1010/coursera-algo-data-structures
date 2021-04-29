72580d04fe6e81c2111418e9e351e791d5dd9214
Anthony Suárez <neolight1010@gmail.com>
2021-04-29 17:30:13 -0500

    Complete get_change()

--- algorithmic-toolbox/week3_greedy_algorithms/1_money_change/change.cpp
+++ algorithmic-toolbox/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
 int get_change(int m) {
-  //write your code here
+  // Minimum number of coins needed to change m using coins of
+  // 1, 5, and 10.
+  int n = 0;
+  
+  if (m >= 10) {
+    n += m / 10;
+    m -= m / 10 * 10;
+  }
+
+  if (m >= 5) {
+    n += m / 5;
+    m -= m / 5 * 5;
+  }
+
+  n += m;
+
   return n;
 }
 
