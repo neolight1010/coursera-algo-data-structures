dad6e032c0736e40452eb89ece0a7a952abc0e61
Anthony Suárez <neolight1010@gmail.com>
2021-04-29 21:09:20 -0500

    Complete fractional knapsack algorithm

--- algorithmic-toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ algorithmic-toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
+struct Item {
+  int value, weight;
+};
+
+bool compare_value_per_weight(Item item1, Item item2) {
+  return (double) item1.value / item1.weight > (double) item2.value / item2.weight;
+}
+
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
+  vector<Item> vw_vec; // Vector containing grouped values and weights.
 
-  // write your code here
+  // Get values per weight
+  for (int i=0; i<weights.size(); i++) { 
+    vw_vec.push_back({values[i], weights[i]});
+  }
+
+  // Sort according to value per weight
+  std::sort(vw_vec.begin(), vw_vec.end(), compare_value_per_weight);
+
+  for (int i=0; i < vw_vec.size(); i++) {
+    if (capacity == 0)
+      break;
+
+    int weight = std::min<int>(capacity, vw_vec[i].weight);
+    value += weight * ((double) vw_vec[i].value / vw_vec[i].weight);
+    capacity -= weight;
+  }
 
   return value;
 }
