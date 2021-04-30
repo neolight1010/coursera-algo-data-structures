709d660ff655ac5ddc3fccead2c9ab024e29075f
Anthony Suárez <neolight1010@gmail.com>
2021-04-30 13:24:13 -0500

    Complete maximum advertisemente revenue algorithm

--- algorithmic-toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ algorithmic-toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,11 +1,14 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
+  sort(a.begin(), a.end());  
+  sort(b.begin(), b.end());
+
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
