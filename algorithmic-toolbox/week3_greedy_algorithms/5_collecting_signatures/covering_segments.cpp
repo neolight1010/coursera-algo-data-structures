0b151b2c4b6db4906b81ef078bf4e0278740894d
Anthony Suárez <neolight1010@gmail.com>
2021-05-02 13:37:17 -0500

    Complete collecting signatures problem.

--- algorithmic-toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
+++ algorithmic-toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
@@ -9,13 +9,32 @@ struct Segment {
   int start, end;
 };
 
+bool greater_start(Segment a , Segment b) {
+  return a.start > b.start;
+}
+
 vector<int> optimal_points(vector<Segment> &segments) {
   vector<int> points;
-  //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+
+  // Base case
+  if (segments.size() == 0)
+    return points;
+
+  std::sort(segments.begin(), segments.end(), greater_start);
+  int point = segments.front().start;
+  points.push_back(point);
+
+  vector<Segment> new_segments;
+  for (int i=0; i < segments.size(); i++) {
+    if (!(point >= segments[i].start && point <= segments[i].end)) {
+      new_segments.push_back(segments[i]);
+    }
   }
+
+  // Recursive call
+  vector<int> new_points = optimal_points(new_segments);
+  points.insert(points.end(), new_points.begin(), new_points.end());
+
   return points;
 }
 
