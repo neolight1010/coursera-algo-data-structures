5e0d68b8ab2d106fcf6880de7732bbc48423bbb8
Anthony Suárez <neolight1010@gmail.com>
2021-05-07 09:00:59 -0500

    Complete maximum salary problem.

--- algorithmic-toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
+++ algorithmic-toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -7,14 +7,38 @@
 using std::vector;
 using std::string;
 
+bool compare_extended(string a, string b) {
+  std::stringstream ss;
+  string new_number_str;
+
+  ss << a;
+  ss << b;
+  ss >> new_number_str;
+
+  int first_number = std::stoi(new_number_str);
+
+  ss.clear();
+  new_number_str.clear();
+  ss << b;
+  ss << a;
+  ss >> new_number_str;
+
+  int second_number = std::stoi(new_number_str);
+
+  return first_number > second_number;
+}
+
 string largest_number(vector<string> a) {
-  //write your code here
-  std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
+  std::sort(a.begin(), a.end(), compare_extended);
+
+  std::stringstream ss;
+  for (string i : a) {
+    ss << i;
   }
+
   string result;
-  ret >> result;
+  ss >> result;
+
   return result;
 }
 
