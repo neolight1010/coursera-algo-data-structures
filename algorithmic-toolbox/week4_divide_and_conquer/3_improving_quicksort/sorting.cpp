69180fd03a874c29f640f0898f850a2e5153dde6
Anthony Suárez <neolight1010@gmail.com>
2021-05-13 13:58:48 -0500

    Implement QuickSort3 algorithm.

--- algorithmic-toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ algorithmic-toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -5,17 +5,27 @@
 using std::vector;
 using std::swap;
 
-int partition2(vector<int> &a, int l, int r) {
+vector<int> partition3(vector<int> &a, int l, int r) {
   int x = a[l];
   int j = l;
+  int x_count = 1;
   for (int i = l + 1; i <= r; i++) {
     if (a[i] <= x) {
       j++;
       swap(a[i], a[j]);
+
+      if (a[i] == x) {
+        swap(a[j], a[l + x_count]);
+        x_count++;
+      }
     }
   }
-  swap(a[l], a[j]);
-  return j;
+
+  for (int i=0; i < x_count; i++) {
+    swap(a[l+i], a[j-i]);
+  }
+
+  return {j - x_count + 1, j};
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
@@ -25,10 +35,10 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  vector<int> m = partition3(a, l, r);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, m[0] - 1);
+  randomized_quick_sort(a, m[1] + 1, r);
 }
 
 int main() {
