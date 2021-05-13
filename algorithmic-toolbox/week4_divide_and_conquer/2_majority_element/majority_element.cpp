edb67dc87b4aaee51e263029758cdb842a653cd0
Anthony Suárez <neolight1010@gmail.com>
2021-05-12 23:35:41 -0500

    Implement majority_element algorightm.

--- algorithmic-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ algorithmic-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -7,7 +7,22 @@ using std::vector;
 int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
-  //write your code here
+
+  int middle_i = (left + right) / 2;
+  int majority_left = get_majority_element(a, left, middle_i);
+  int majority_right = get_majority_element(a, middle_i, right);
+
+  int count_left = 0;
+  int count_right = 0;
+
+  for (int i=left; i < right; i++) {
+    if (a[i] == majority_left) count_left++;
+    if (a[i] == majority_right) count_right++;
+
+    if (count_left > (right - left) / 2) return majority_left;
+    if (count_right > (right - left) / 2) return majority_right;
+  }
+
   return -1;
 }
 
