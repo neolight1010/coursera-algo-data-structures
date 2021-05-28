dc4c5c350e85987c0dc09b13528d511dcb5e2a10
Anthony Suárez <neolight1010@gmail.com>
2021-05-28 15:04:01 -0500

    Write change_dp algorithm.

--- algorithmic-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ algorithmic-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <limits>
+#include <vector>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+/** Get minimum number of coins to change w with coins 1, 3, 4. */
+int get_min_change(int m, std::vector<int> coins = {1, 3, 4}) {
+  std::vector<int> min_changes = {0};
+
+  for (int i = 1; i <= m; i++) {
+    min_changes.push_back(i);
+
+    for (int coin : coins) {
+      if (i >= coin) {
+        int change = min_changes[i - coin] + 1;
+
+        if (change < min_changes[i]) {
+          min_changes[i] = change;
+        }
+      }
+    }
+  }
+
+  return min_changes[m];
 }
 
 int main() {
   int m;
   std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  std::cout << get_min_change(m) << '\n';
 }
