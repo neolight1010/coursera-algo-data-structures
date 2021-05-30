c5ed33d3a16129db530803b8d69fb1edf949123d
Anthony Suárez <neolight1010@gmail.com>
2021-05-30 18:23:12 -0500

    Complete primitive calculator algorithm.

--- algorithmic-toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ algorithmic-toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -1,22 +1,64 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using std::vector;
 
 vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
+  vector<int> min_operations = {0};
+  vector<int> prev_nums = {0};
+
+  for (int i = 1; i <= n; i++) {
+    if (i == 1) {
+      min_operations.push_back(0);
+      prev_nums.push_back(1);
+      continue;
     }
+
+    int num_x2, num_x3, num_p1;
+    bool found_x2 = false;
+    bool found_x3 = false;
+
+    num_p1 = min_operations[i - 1] + 1;
+
+    if (i % 2 == 0) {
+      found_x2 = true;
+      num_x2 = min_operations[i / 2] + 1;
+    }
+
+    if (i % 3 == 0) {
+      found_x3 = true;
+      num_x3 = min_operations[i / 3] + 1;
+    }
+
+    // Change in case i wasn't divisible by 2 and 3.
+    if (!found_x2)
+      num_x2 = num_p1 + 1;
+
+    if (!found_x3)
+      num_x3 = num_p1 + 1;
+
+    int min_op = std::min(num_x2, num_x3);
+    min_op = std::min(min_op, num_p1);
+
+    min_operations.push_back(min_op);
+
+    // Detect previous number
+    if (min_op == num_p1)
+      prev_nums.push_back(i - 1);
+    else if (min_op == num_x2)
+      prev_nums.push_back(i / 2);
+    else if (min_op == num_x3)
+      prev_nums.push_back(i / 3);
   }
-  reverse(sequence.begin(), sequence.end());
+
+  vector<int> sequence = {n};
+  int c = n;
+  while (c != 1) {
+    sequence.insert(sequence.begin(), prev_nums[c]);
+    c = prev_nums[c];
+  }
+
   return sequence;
 }
 
