aeed87c32175e27e5808fd02d7684d67b283c5f3
Anthony Suáreź <neolight1010@gmail.com>
2021-10-06 21:53:21 -0500

    Complete w1.2

--- algorithms-on-graphs/week1_graph_decomposition1/2_adding_exits_to_maze/ConnectedComponents.java
+++ algorithms-on-graphs/week1_graph_decomposition1/2_adding_exits_to_maze/ConnectedComponents.java
@@ -2,28 +2,55 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConnectedComponents {
-    private static int numberOfComponents(ArrayList<Integer>[] adj) {
-        int result = 0;
-        //write your code here
-        return result;
+    private static ArrayList<Boolean> explored = new ArrayList<Boolean>();
+    
+    private static int numberOfComponents(ArrayList<ArrayList<Integer>> adj) {
+        int n = 0;
+
+        while (explored.contains(false)) {
+            int currentNode = explored.indexOf(false);
+            explore(currentNode, adj);
+            n++;
+        }
+
+        return n;
+    }
+
+    private static void explore(int node, ArrayList<ArrayList<Integer>> adj) {
+        explored.set(node, true);
+
+        for (int neighbor: adj.get(node)) {
+            if (!explored.get(neighbor)) {
+                explore(neighbor, adj);
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
+            adj.add(new ArrayList<Integer>());
         }
+
         for (int i = 0; i < m; i++) {
             int x, y;
             x = scanner.nextInt();
             y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            adj[y - 1].add(x - 1);
+            adj.get(x - 1).add(y - 1);
+            adj.get(y - 1).add(x - 1);
         }
+
+        // Fill notExplored list
+        for (int i = 0; i < n; i++) {
+            explored.add(false);
+        }
+
         System.out.println(numberOfComponents(adj));
+        scanner.close();
     }
 }
 
