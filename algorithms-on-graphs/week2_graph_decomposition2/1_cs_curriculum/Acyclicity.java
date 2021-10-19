bfb535164569cd10d1abe2a4f4fc6b995689fe61
Anthony Suárez <neolight1010@gmail.com>
2021-10-19 08:02:06 -0500

    Complete w2.1

--- algorithms-on-graphs/week2_graph_decomposition2/1_cs_curriculum/Acyclicity.java
+++ algorithms-on-graphs/week2_graph_decomposition2/1_cs_curriculum/Acyclicity.java
@@ -1,26 +1,72 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Acyclicity {
-    private static int acyclic(ArrayList<Integer>[] adj) {
-        //write your code here
+    static ArrayList<Integer> notExplored = new ArrayList<Integer>();
+    static Stack<Integer> routeStack = new Stack<Integer>();
+
+    private static int acyclic(ArrayList<ArrayList<Integer>> adj) {
+        while (!notExplored.isEmpty()) {
+            int current = notExplored.get(0);
+
+            // If a cycle is found.
+            if (explore(current, adj)) {
+                return 1;
+            }
+        }
+
         return 0;
+    }
+
+    /*
+     * Returns true if a cycle was found.
+     */
+    private static boolean explore(int node, ArrayList<ArrayList<Integer>> adj) {
+        notExplored.remove(notExplored.indexOf(node));
+        routeStack.push(node);
+
+        for (int neighbor : adj.get(node)) {
+            if (routeStack.contains(neighbor)) {
+                return true;
+            }
+
+            if (notExplored.contains(neighbor)) {
+                boolean cycleInExplore = explore(neighbor, adj);
+
+                if (cycleInExplore) {
+                    return true;
+                }
+            }
+        }
+
+        routeStack.pop();
+
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
+            adj.add(new ArrayList<Integer>());
+            notExplored.add(i);
         }
+
         for (int i = 0; i < m; i++) {
             int x, y;
             x = scanner.nextInt();
             y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
+            adj.get(x - 1).add(y - 1);
         }
+
+        scanner.close();
+
         System.out.println(acyclic(adj));
     }
 }
