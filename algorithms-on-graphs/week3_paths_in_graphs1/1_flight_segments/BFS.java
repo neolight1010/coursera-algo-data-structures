ecb6ca2e92b775bf9d71c59574407b92311a0190
Anthony Suárez <neolight1010@gmail.com>
2021-10-28 21:31:06 -0500

    Complete w3.1

--- algorithms-on-graphs/week3_paths_in_graphs1/1_flight_segments/BFS.java
+++ algorithms-on-graphs/week3_paths_in_graphs1/1_flight_segments/BFS.java
@@ -1,32 +1,57 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class BFS {
-    private static int distance(ArrayList<Integer>[] adj, int s, int t) {
-        //write your code here
-        return -1;
+    private static int distance(ArrayList<ArrayList<Integer>> adj, int s, int t) {
+        ArrayList<Integer> dist = new ArrayList<>();
+        for (int i = 0; i < adj.size(); i++) {
+            dist.add(-1);
+        }
+
+        dist.set(s, 0);
+
+        ArrayList<Integer> exploreQueue = new ArrayList<>();
+        exploreQueue.add(s);
+
+        while (!exploreQueue.isEmpty()) {
+            int node = exploreQueue.remove(0);
+
+            for (int neighbor : adj.get(node)) {
+                if (dist.get(neighbor) == -1) {
+                    exploreQueue.add(neighbor);
+                    dist.set(neighbor, dist.get(node) + 1);
+                }
+            }
+        }
+
+        return dist.get(t);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
+            adj.add(new ArrayList<>());
         }
         for (int i = 0; i < m; i++) {
             int x, y;
             x = scanner.nextInt();
             y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            adj[y - 1].add(x - 1);
+
+            adj.get(x - 1).add(y - 1);
+            adj.get(y - 1).add(x - 1);
         }
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
+        
+        scanner.close();
+
         System.out.println(distance(adj, x, y));
     }
 }
-
