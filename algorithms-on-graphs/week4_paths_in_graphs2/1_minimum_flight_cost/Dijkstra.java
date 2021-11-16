e7c72a2b5aa7e0c3ea620defdce6e669d60c60c6
Anthony Suárez <neolight1010@gmail.com>
2021-11-16 17:50:20 -0500

    Complete w4.1.

--- algorithms-on-graphs/week4_paths_in_graphs2/1_minimum_flight_cost/Dijkstra.java
+++ algorithms-on-graphs/week4_paths_in_graphs2/1_minimum_flight_cost/Dijkstra.java
@@ -1,30 +1,79 @@
 import java.util.*;
 
 public class Dijkstra {
-    private static long distance(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, int t) {
-        return -1;
+    private static long distance(ArrayList<ArrayList<Integer>> adj, ArrayList<ArrayList<Integer>> cost, int s, int t) {
+        boolean[] visited = new boolean[adj.size()];
+
+        ArrayList<Integer> dist = new ArrayList<>();
+
+        for (int i = 0; i < adj.size(); i++) {
+            dist.add(-1);
+        }
+
+        dist.set(s, 0);
+
+        // Comparator
+        Comparator<Integer> distComparator = (n1, n2) -> {
+            return dist.get(n1) - dist.get(n2);
+        };
+
+        // Start algorithm
+        PriorityQueue<Integer> queue = new PriorityQueue<>(distComparator);
+        queue.add(s);
+
+        while (!queue.isEmpty()) {
+            int node = queue.remove();
+            int nodeDist = dist.get(node);
+
+            for (int neighbor : adj.get(node)) {
+                int edgeCost = cost.get(node).get(neighbor);
+
+                if ((nodeDist + edgeCost < dist.get(neighbor)) || dist.get(neighbor) < 0) {
+                    dist.set(neighbor, nodeDist + edgeCost);
+
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        return dist.get(t);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        // TODO: make cost a HashMap?
+        ArrayList<ArrayList<Integer>> cost = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-            cost[i] = new ArrayList<Integer>();
+            adj.add(new ArrayList<Integer>());
+            cost.add(new ArrayList<Integer>());
+
+            for (int j = 0; j < n; j++) {
+                cost.get(i).add(-1);
+            }
         }
+
         for (int i = 0; i < m; i++) {
             int x, y, w;
             x = scanner.nextInt();
             y = scanner.nextInt();
             w = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            cost[x - 1].add(w);
+
+            adj.get(x - 1).add(y - 1);
+            cost.get(x - 1).set(y - 1, w);
         }
+
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
+
+        scanner.close();
+
         System.out.println(distance(adj, cost, x, y));
     }
 }
