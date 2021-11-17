28a97711343c6885103eafc0c27ca9b618abb757
Anthony Suárez <neolight1010@gmail.com>
2021-11-17 09:14:13 -0500

    Complete w4.2.

--- algorithms-on-graphs/week4_paths_in_graphs2/2_detecting_anomalies/NegativeCycle.java
+++ algorithms-on-graphs/week4_paths_in_graphs2/2_detecting_anomalies/NegativeCycle.java
@@ -2,29 +2,74 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NegativeCycle {
-    private static int negativeCycle(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost) {
-        // write your code here
+    private static int negativeCycle(ArrayList<ArrayList<Integer>> adj, ArrayList<ArrayList<Integer>> cost) {
+        ArrayList<Integer> dist = new ArrayList<>();
+
+        // TODO: Use Integer.MAX_VALUE and handle it differently
+        for (int i = 0; i < adj.size(); i++) {
+            dist.add(Integer.MAX_VALUE / 2);
+        }
+
+        // TODO: account for multiple SCCs?
+        dist.set(0, 0);
+
+        // V - 1 times
+        for (int i = 0; i < adj.size() - 1; i++) {
+            for (int node = 0; node < adj.size(); node++) {
+                for (int neighbor : adj.get(node)) {
+                    int edgeCost = cost.get(node).get(neighbor);
+
+                    if (dist.get(neighbor) > dist.get(node) + edgeCost) {
+                        dist.set(neighbor, dist.get(node) + edgeCost);
+                    }
+                }
+            }
+        }
+
+        // For each edge
+        for (int node = 0; node < adj.size(); node++) {
+            for (int neighbor : adj.get(node)) {
+                int edgeCost = cost.get(node).get(neighbor);
+
+                if (dist.get(neighbor) > dist.get(node) + edgeCost) {
+                    return 1;
+                }
+            }
+        }
+
         return 0;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> cost = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-            cost[i] = new ArrayList<Integer>();
+            adj.add(new ArrayList<Integer>());
+            cost.add(new ArrayList<Integer>());
+
+            for (int j = 0; j < n; j++) {
+                cost.get(i).add(null);
+            }
         }
+
         for (int i = 0; i < m; i++) {
             int x, y, w;
             x = scanner.nextInt();
             y = scanner.nextInt();
             w = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            cost[x - 1].add(w);
+
+            adj.get(x - 1).add(y - 1);
+            cost.get(x - 1).set(y - 1, w);
         }
+
+        scanner.close();
+
         System.out.println(negativeCycle(adj, cost));
     }
 }
