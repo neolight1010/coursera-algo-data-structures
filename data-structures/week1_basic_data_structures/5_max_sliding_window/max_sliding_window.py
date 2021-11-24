dd81aa2f14c60c55c4c97f498a5aaccb3c060e67
Anthony Suárez <neolight1010@gmail.com>
2021-11-23 19:00:43 -0500

    Complete data_structures w1.5

--- data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,12 +1,36 @@
-# python3
+from typing import List
 
 
-def max_sliding_window_naive(sequence, m):
-    maximums = []
-    for i in range(len(sequence) - m + 1):
-        maximums.append(max(sequence[i:i + m]))
+def max_sliding_window_naive(sequence: List[int], m: int) -> List[int]:
+    dequeue: list[int] = []
+    max_list: list[int] = []
 
-    return maximums
+    # Initial dequeue
+    for i in range(m):
+        current = sequence[i]
+
+        while dequeue and sequence[dequeue[0]] < current:
+            dequeue.pop(0)
+
+        dequeue.insert(0, i)
+
+    max_list.append(sequence[dequeue[-1]])
+
+    for i in range(m, len(sequence)):
+        out_of_window_i = i - m
+        current = sequence[i]
+
+        while dequeue and dequeue[-1] <= out_of_window_i:
+            dequeue.pop(-1)
+
+        while dequeue and sequence[dequeue[0]] < current:
+            dequeue.pop(0)
+
+        dequeue.insert(0, i)
+        max_list.append(sequence[dequeue[-1]])
+
+    return max_list
+
 
 if __name__ == '__main__':
     n = int(input())
