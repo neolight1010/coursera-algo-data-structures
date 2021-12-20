a392070fa3ab69e4f297e8790987d0e420583252
Anthony Suárez <neolight1010@gmail.com>
2021-12-20 14:07:04 -0500

    Complete data_structs w2.3. - merging tables.

--- data-structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ data-structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -1,6 +1,3 @@
-# python3
-
-
 class Database:
     def __init__(self, row_counts):
         self.row_counts = row_counts
@@ -9,21 +6,55 @@ class Database:
         self.ranks = [1] * n_tables
         self.parents = list(range(n_tables))
 
-    def merge(self, src, dst):
+    def merge(self, src, dst) -> bool:
         src_parent = self.get_parent(src)
         dst_parent = self.get_parent(dst)
 
         if src_parent == dst_parent:
             return False
 
+        # TODO: ranks are wrongly updated.
+
         # merge two components
         # use union by rank heuristic
         # update max_row_count with the new maximum table size
+        if self.ranks[src_parent] > self.ranks[dst_parent]:
+            self.parents[dst_parent] = src_parent
+            self.ranks[src_parent] += 1
+
+            # Update row counts
+            self.row_counts[src_parent] += self.row_counts[dst_parent]
+            self.row_counts[dst_parent] = 0
+        else:
+            self.parents[src_parent] = dst_parent
+            self.ranks[dst_parent] += 1
+
+            # Update row counts
+            self.row_counts[dst_parent] += self.row_counts[src_parent]
+            self.row_counts[src_parent] = 0
+
+
+        # Update max value
+        if self.row_counts[dst_parent] > self.max_row_count:
+            new_max = self.row_counts[dst_parent]
+            self.max_row_count = new_max
+
+        if self.row_counts[src_parent] > self.max_row_count:
+            new_max = self.row_counts[src_parent]
+            self.max_row_count = new_max
+
         return True
 
     def get_parent(self, table):
         # find parent and compress path
-        return self.parents[table]
+        current = table
+        while not (current == self.parents[current]):
+            parent = self.parents[current]
+            current = parent
+
+        self.parents[table] = current
+
+        return current
 
 
 def main():
@@ -31,7 +62,7 @@ def main():
     counts = list(map(int, input().split()))
     assert len(counts) == n_tables
     db = Database(counts)
-    for i in range(n_queries):
+    for _ in range(n_queries):
         dst, src = map(int, input().split())
         db.merge(dst - 1, src - 1)
         print(db.max_row_count)
