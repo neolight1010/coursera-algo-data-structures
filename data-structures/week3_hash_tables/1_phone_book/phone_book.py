aa81ee424568a5071365c440946b69a275fadf44
Anthony Suárez <neolight1010@gmail.com>
2022-02-05 21:14:07 -0500

    Complete data-structs w3.1: phone book.

--- data-structures/week3_hash_tables/1_phone_book/phone_book.py
+++ data-structures/week3_hash_tables/1_phone_book/phone_book.py
@@ -1,45 +1,35 @@
-# python3
+from typing import List
 
 class Query:
     def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
+        self.type: str = query[0] # add, del or find
+        self.number: int = int(query[1])
+
         if self.type == 'add':
-            self.name = query[2]
+            self.name: str = query[2]
 
-def read_queries():
+def read_queries() -> List[Query]:
     n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    return [Query(input().split()) for _ in range(n)]
 
-def write_responses(result):
+def write_responses(result: List[str]) -> None:
     print('\n'.join(result))
 
-def process_queries(queries):
-    result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+def process_queries(queries: List[Query]) -> List[str]:
+    contacts: dict[int, str] = {}
+    result: List[str] = []
+
+    for query in queries:
+        if query.type == "add":
+            contacts[query.number] = query.name
+        elif query.type == "del":
+            contacts.pop(query.number, None)
+        elif query.type == "find":
+            if query.number in contacts:
+                result.append(contacts[query.number])
+            else:
+                result.append('not found')
+
     return result
 
 if __name__ == '__main__':
