c33d6ec49d9743b95b09f0b6e344ae264081ea0d
Anthony Suárez <neolight1010@gmail.com>
2022-02-05 22:23:16 -0500

    Complete data-structs w3.2: hash chains.

--- data-structures/week3_hash_tables/2_hash_chains/hash_chains.py
+++ data-structures/week3_hash_tables/2_hash_chains/hash_chains.py
@@ -1,64 +1,84 @@
-# python3
+from typing import List
+
+
+class HashMap():
+    """HashMap of strings. more like a Set, as it only uses keys, not values."""
+
+    _P = 1000000007 
+    """Prime number used for the hash function."""
+
+    _X = 263
+    """Multiplier number used for the hash function."""
+
+    def __init__(self, buckets_n: int):
+        self._m = buckets_n
+        """Numbers of buckets for the hash function."""
+
+        self.chains: List[List[str]] = [[] for _ in range(self._m)]
+
+    def _hash(self, s: str):
+        """Get the hash number of a string."""
+        x = HashMap._X
+        p = HashMap._P
+
+        s_ = sum([ord(c) * x ** i for i, c in enumerate(s)])
+        return s_ % p % self._m
+
+    def add(self, s: str):
+        """Add a string to the HashMap."""
+        i = self._hash(s)
+
+        if not s in self.chains[i]:
+            self.chains[i].insert(0, s)
+
+    def remove(self, s: str):
+        """Remove a string from the HashMap."""
+        if self.contains(s):
+            i = self._hash(s)
+            self.chains[i].remove(s)
+
+    def contains(self, s: str) -> bool:
+        """Check if a string exists in the HashMap."""
+        i = self._hash(s)
+
+        return s in self.chains[i]
+
 
 class Query:
+    def __init__(self, instruction: str):
+        tokens = instruction.split()
 
-    def __init__(self, query):
-        self.type = query[0]
-        if self.type == 'check':
-            self.ind = int(query[1])
-        else:
-            self.s = query[1]
+        self.type = tokens[0]
+        self.arg = tokens[1]
 
+    def process(self, hash_map: HashMap):
+        if self.type == "add":
+            hash_map.add(self.arg)
 
-class QueryProcessor:
-    _multiplier = 263
-    _prime = 1000000007
+        elif self.type == "del":
+            hash_map.remove(self.arg)
 
-    def __init__(self, bucket_count):
-        self.bucket_count = bucket_count
-        # store all strings in one list
-        self.elems = []
-
-    def _hash_func(self, s):
-        ans = 0
-        for c in reversed(s):
-            ans = (ans * self._multiplier + ord(c)) % self._prime
-        return ans % self.bucket_count
-
-    def write_search_result(self, was_found):
-        print('yes' if was_found else 'no')
-
-    def write_chain(self, chain):
-        print(' '.join(chain))
-
-    def read_query(self):
-        return Query(input().split())
-
-    def process_query(self, query):
-        if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
-        else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+        elif self.type == "find":
+            if hash_map.contains(self.arg):
+                print("yes")
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                print("no")
 
-    def process_queries(self):
-        n = int(input())
-        for i in range(n):
-            self.process_query(self.read_query())
+        elif self.type == "check":
+            i = int(self.arg)
+            print(" ".join(hash_map.chains[i]))
 
-if __name__ == '__main__':
-    bucket_count = int(input())
-    proc = QueryProcessor(bucket_count)
-    proc.process_queries()
+
+if __name__ == "__main__":
+    queries: List[Query] = []
+
+    buckets_n = int(input())
+    hash_map = HashMap(buckets_n)
+
+    queries_n = int(input())
+
+    for _ in range(queries_n):
+        queries.append(Query(input()))
+
+    for query in queries:
+        query.process(hash_map)
