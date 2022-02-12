9f69ade2c0c1d01e9f620c22e6b9507436c80c00
Anthony Suárez <neolight1010@gmail.com>
2022-02-12 15:07:53 -0500

    Complete data-structs w4.3: is_bst_hard.

--- data-structures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ data-structures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -1,23 +1,58 @@
-#!/usr/bin/python3
-
 import sys, threading
+from typing import List, Tuple, Union
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+MAX_INT: int = pow(2, 31)
+MIN_INT: int = -MAX_INT
+
+sys.setrecursionlimit(10**9) # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
+
+
+class Node:
+    def __init__(self):
+        self.key: int = 0
+        self.left: Union[Node, None] = None
+        self.right: Union[Node, None] = None
+
+
+def is_binary_search_tree(node: Union[Node, None],
+                          max_lim: int = MAX_INT,
+                          min_lim: int = MIN_INT) -> bool:
+    if node is None: return True
+
+    if (node.left and node.left.key >= node.key) \
+       or node.key >= max_lim:
+        return False
+
+    if (node.right and node.right.key < node.key) \
+       or node.key < min_lim:
+        return False
+
+    return is_binary_search_tree(node.left, min(max_lim, node.key), min_lim) and is_binary_search_tree(node.right, max_lim, max(min_lim, node.key))
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    n = int(sys.stdin.readline().strip())
+    queries: List[Tuple[int, int, int]] = []
+
+    tree: List[Node] = []
+
+    for _ in range(n):
+        queries.append(tuple(map(int, sys.stdin.readline().strip().split())))
+        tree.append(Node())
+
+    # Set tree's nodes' children
+    for i, query in enumerate(queries):
+        key, left_i, right_i = query
+
+        tree[i].key = key
+        tree[i].left = tree[left_i] if left_i >= 0 else None
+        tree[i].right = tree[right_i] if right_i >= 0 else None
+
+    if len(tree) == 0 or is_binary_search_tree(tree[0]):
+        print("CORRECT")
+    else:
+        print("INCORRECT")
 
 threading.Thread(target=main).start()
