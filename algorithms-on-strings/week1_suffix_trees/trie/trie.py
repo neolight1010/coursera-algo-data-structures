bddbbf1d40dfcd1997145efb0a87ca8d298d5821
Anthony Suárez <neolight1010@gmail.com>
2022-02-24 22:38:33 -0500

    Complete algo-on-str w1.1: trie.

--- algorithms-on-strings/week1_suffix_trees/trie/trie.py
+++ algorithms-on-strings/week1_suffix_trees/trie/trie.py
@@ -1,23 +1,82 @@
-#Uses python3
-import sys
+# python3
 
-# Return the trie built from patterns
-# in the form of a dictionary of dictionaries,
-# e.g. {0:{'A':1,'T':2},1:{'C':3}}
-# where the key of the external dictionary is
-# the node ID (integer), and the internal dictionary
-# contains all the trie edges outgoing from the corresponding
-# node, and the keys are the letters on those edges, and the
-# values are the node IDs to which these edges lead.
-def build_trie(patterns):
-    tree = dict()
-    # write your code here
-    return tree
+class Node:
+    def __init__(self, value: str):
+        self.value = value
+        self._children = {}
+
+    def add_child(self, node):
+        self._children[node.value] = node
+
+    def child_by_value(self, value):
+        return self._children[value]
+
+    def has_child_with_value(self, value):
+        return value in self._children.keys()
+
+    def children_nodes(self):
+        return self._children.values()
 
 
-if __name__ == '__main__':
-    patterns = sys.stdin.read().split()[1:]
-    tree = build_trie(patterns)
-    for node in tree:
-        for c in tree[node]:
-            print("{}->{}:{}".format(node, tree[node][c], c))
+class Trie:
+    def __init__(self):
+        self.root = Node("")
+        self._all_nodes = [self.root]
+
+    def add_pattern(self, pattern: str) -> None:
+        current_node = self.root
+
+        for char in pattern:
+            if current_node.has_child_with_value(char):
+                current_node = current_node.child_by_value(char)
+                continue
+
+            new_node = Node(char)
+
+            current_node.add_child(new_node)
+            self._all_nodes.append(new_node)
+
+            current_node = new_node
+
+
+    def get_adjacency(self):
+        result = []
+
+        indices = self.__build_node_indices__()
+        queue = [self.root]
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+
+            for child in current_node.children_nodes():
+                edge = (indices[current_node], indices[child])
+                result.append((edge, child.value))
+
+                queue.append(child)
+
+        return result
+
+
+    def __build_node_indices__(self):
+        result = {}
+
+        for i, node in enumerate(self._all_nodes):
+            result[node] = i
+
+        return result
+
+
+
+
+if __name__ == "__main__":
+    trie = Trie()
+    n_of_patterns = int(input())
+    
+    for _ in range(n_of_patterns):
+        pattern = input()
+        trie.add_pattern(pattern)
+
+    adjacency = trie.get_adjacency()
+    
+    for e in adjacency:
+        print("{}->{}:{}".format(e[0][0], e[0][1], e[1]))
