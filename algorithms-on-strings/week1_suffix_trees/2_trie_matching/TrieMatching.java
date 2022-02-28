30d511be47f9e35cebb1a2994e714ace3ef18a4a
Anthony Suárez <neolight1010@gmail.com>
2022-02-28 10:41:58 -0500

    Complete algo-on-str w1.2: trie matching.

--- algorithms-on-strings/week1_suffix_trees/2_trie_matching/TrieMatching.java
+++ algorithms-on-strings/week1_suffix_trees/2_trie_matching/TrieMatching.java
@@ -1,64 +1,242 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-class Node
-{
-	public static final int Letters =  4;
-	public static final int NA      = -1;
-	public int next [];
+class Trie {
+	private Node root = new Node(' ');
 
-	Node ()
-	{
-		next = new int [Letters];
-		Arrays.fill (next, NA);
+	public Node getRoot() {
+		return root;
+	}
+
+	public void addPattern(String pattern) {
+		Node currentNode = root;
+
+		char[] patternChars = pattern.toCharArray();
+		for (int i = 0; i < patternChars.length; i++) {
+			char c = patternChars[i];
+			boolean isLastChar = i == patternChars.length - 1;
+
+			Node matchingChild = currentNode.childByValue(c);
+
+			if (matchingChild == null) {
+				Node newNode = new Node(c);
+
+				if (isLastChar) {
+					newNode.setIsLeaf(true);
+				}
+
+				currentNode.addChild(newNode);
+				currentNode = newNode;
+
+				continue;
+			}
+
+			if (isLastChar) {
+				matchingChild.setIsLeaf(true);
+			}
+
+			currentNode = matchingChild;
+		}
+	}
+
+	public List<Integer> match(String text) {
+		List<Integer> matches = new ArrayList<>();
+		String currentText = text;
+
+		for (int i = 0; true; i++) {
+			if (currentText.length() == 0) {
+				break;
+			}
+
+			boolean foundLeaf = traverseUntilLeaf(currentText);
+			if (foundLeaf) {
+				matches.add(i);
+			}
+
+			currentText = currentText.substring(1);
+		}
+
+		return matches;
+	}
+
+	private boolean traverseUntilLeaf(String text) {
+		Node currentNode = root;
+
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+
+			Node matchingChild = currentNode.childByValue(c);
+
+			if (matchingChild == null) {
+				return false;
+			}
+
+			if (matchingChild != null) {
+				if (matchingChild.isLeaf()) {
+					return true;
+				}
+
+				currentNode = matchingChild;
+				continue;
+			}
+		}
+
+		return false;
 	}
 }
 
-public class TrieMatching implements Runnable {
-	int letterToIndex (char letter)
-	{
-		switch (letter)
+class Node {
+	public char value;
+	private List<Node> children = new ArrayList<>();
+	private boolean isLeaf = false;
+
+	public Node(char value) {
+		this.value = value;
+	}
+
+	public void setIsLeaf(boolean isLeaf) {
+		this.isLeaf = isLeaf;
+	}
+
+
+	public void addChild(Node node) {
+		children.add(node);
+	}
+
+	public int childrenSize() {
+		return children.size();
+	}
+
+	public Node childByValue(char value) {
+		for (Node child : children) {
+			if (child.value == value) {
+				return child;
+			}
+		}
+
+		return null;
+	}
+
+	public boolean isLeaf() {
+		return children.isEmpty() || this.isLeaf;
+	}
+}
+
+class Testing {
+	public static void testAll() {
+		testTrie();
+	}
+
+	private static void testTrie() {
 		{
-			case 'A': return 0;
-			case 'C': return 1;
-			case 'G': return 2;
-			case 'T': return 3;
-			default: assert (false); return Node.NA;
+			Trie trie = new Trie();
+			trie.addPattern("abcd");
+
+			assert trie.getRoot().childrenSize() == 1 :
+				"Adding 1 pattern.";
+		}
+
+		{
+			Trie trie = new Trie();
+			trie.addPattern("aaaa");
+			trie.addPattern("bbbb");
+
+			assert trie.getRoot().childrenSize() == 2 :
+				"Adding 2 patterns with distinct values.";
+
+			assert trie.getRoot().childByValue('a').childrenSize() == 1;
+			assert trie.getRoot().childByValue('b').childrenSize() == 1;
+		}
+
+		{
+			Trie trie = new Trie();
+			trie.addPattern("zbaa");
+			trie.addPattern("z");
+
+			assert trie.getRoot().childrenSize() == 1 :
+				"Adding 2 patterns with similar values.";
+
+			assert trie.getRoot().childByValue('z').isLeaf();
+		}
+
+		{
+			Trie trie = new Trie();
+			trie.addPattern("abcd");
+
+			assert trie.match("efgh").equals(new ArrayList<>()) :
+				"Matching no patterns";
+		}
+
+		{
+			Trie trie = new Trie();
+			trie.addPattern("efg");
+
+			List<Integer> expected = new ArrayList<>(Arrays.asList(4));
+			assert trie.match("abcdefgh").equals(expected) :
+				"Matching 1 pattern";
+		}
+
+		{
+			Trie trie = new Trie();
+			trie.addPattern("efg");
+			trie.addPattern("cde");
+			trie.addPattern("lll");
+			trie.addPattern("llf");
+
+			List<Integer> expected = new ArrayList<>(Arrays.asList(0, 2, 6, 7, 8));
+			assert trie.match("cdefghllllf").equals(expected) :
+				"Matching multiple patterns";
 		}
 	}
+}
 
-	List <Integer> solve (String text, int n, List <String> patterns) {
-		List <Integer> result = new ArrayList <Integer> ();
+class TrieMatching implements Runnable {
+	private String text;
+	private Trie trie = new Trie();
 
-		// write your code here
-
-		return result;
+	public static void main(String[] args) {
+		new Thread(new TrieMatching()).start();
 	}
 
-	public void run () {
+	public void run() {
+		Testing.testAll();
+
 		try {
-			BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
-			String text = in.readLine ();
-		 	int n = Integer.parseInt (in.readLine ());
-		 	List <String> patterns = new ArrayList <String> ();
-			for (int i = 0; i < n; i++) {
-				patterns.add (in.readLine ());
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+			text = in.readLine();
+
+			int nOfPatterns = Integer.parseInt(in.readLine());
+
+			for (int i = 0; i < nOfPatterns; i++) {
+				String pattern = in.readLine();
+
+				trie.addPattern(pattern);
 			}
 
-			List <Integer> ans = solve (text, n, patterns);
-
-			for (int j = 0; j < ans.size (); j++) {
-				System.out.print ("" + ans.get (j));
-				System.out.print (j + 1 < ans.size () ? " " : "\n");
-			}
-		}
-		catch (Throwable e) {
-			e.printStackTrace ();
-			System.exit (1);
+			solveAndShowResults();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
 		}
 	}
 
-	public static void main (String [] args) {
-		new Thread (new TrieMatching ()).start ();
+	private void solveAndShowResults() {
+		List<Integer> matches = trie.match(text);
+		Collections.sort(matches);
+
+		List<String> matchesStr = new ArrayList<>();
+
+		for (int match : matches) {
+			matchesStr.add(Integer.toString(match));
+		}
+
+		System.out.println(String.join(" ", matchesStr));
+
 	}
 }
